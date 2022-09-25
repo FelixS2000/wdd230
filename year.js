34c1b1396c8e658993a76a67c9c797b76919f778
FelixS2000 <113263790+FelixS2000@users.noreply.github.com>
2022-09-25 00:07:14 -0400

    Add files via upload
    Signed-off-by: FelixS2000 <113263790+FelixS2000@users.noreply.github.com>

---
+++ year.js
@@ -0,0 +1,9 @@
+var date = new Date();
+var year = date.getFullYear();
+var day = date.getDay();
+var month = date.getMonth();
+var dates = date.getDate();
+var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+document.getElementById("currentTime").innerHTML = month + '/' + day + '/' + year;
+document.getElementById("currentYear").innerHTML = year;
+document.getElementById("currentHour").innerHTML = time;
