cdc402ec6f1439ee190702cd35ecfd013b02c9cc
FelixS2000 <guz22002@byui.edu>
2022-10-22 17:59:56 -0400

    init commit

--- chamber/scripts/time.js
+++ chamber/scripts/time.js
@@ -1,4 +1,9 @@
 var date = new Date();
 var year = date.getFullYear();
+const navbutton = document.querySelector('.navig');
+const mainnav = document.querySelector('.navigation');
+
+
+
 document.getElementById("currentYear").innerHTML = year;
 document.getElementById("lastModified").innerHTML = date;
