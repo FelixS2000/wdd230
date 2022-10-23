84c6ecfc231e08acfa344654ad7300414b77a803
FelixS2000 <guz22002@byui.edu>
2022-10-23 14:01:31 -0400

    init commit

--- chamber/scripts/time.js
+++ chamber/scripts/time.js
@@ -1,9 +1,14 @@
 var date = new Date();
 var year = date.getFullYear();
-const navbutton = document.querySelector('.navig');
-const mainnav = document.querySelector('.navigation');
+var time = date.toLocaleString();
+const hambutton = document.querySelector('.navig');
+const mainnav = document.querySelector('.navigation')
 
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
 document.getElementById("currentYear").innerHTML = year;
 document.getElementById("lastModified").innerHTML = date;
+document.getElementById("lastTime").innerHTML = time;
