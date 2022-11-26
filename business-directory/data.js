4275e95ad739db5842d55ce6fc2678ef3ec9a5f7
FelixS2000 <113263790+FelixS2000@users.noreply.github.com>
2022-11-26 18:15:21 -0400

    Add files via upload
    Signed-off-by: FelixS2000 <113263790+FelixS2000@users.noreply.github.com>

---
+++ business-directory/data.js
@@ -0,0 +1,18 @@
+const company1 = '{"company":"Rica","address":"KM 6 1/2 Autopista Duarte, EN, Santo Domingo","phoneNumber":"809-563-7422"}'
+const company1text = JSON.parse(company1)
+const company1image = URL()
+const company2 = 
+
+const company3 =
+
+const company4 =
+
+const company5 =
+
+const company6 =
+
+const company7 =
+
+const company8 =
+
+const company9 =
