9bf44dcdfb2ebffd04659d9705a3ce05ce1534fd
FelixS2000 <113263790+FelixS2000@users.noreply.github.com>
2022-11-26 18:27:59 -0400

    Add files via upload
    Signed-off-by: FelixS2000 <113263790+FelixS2000@users.noreply.github.com>

---
+++ join-page/join.js
@@ -0,0 +1,7 @@
+const fname = document.getElementsByClassName("firstName");
+const lname = document.getElementsByClassName("lastName");
+const email = document.getElementsByClassName("emailUser");
+const phoneNumber = document.getElementsByClassName();
+function submit(){
+    document.getElementById("Submit").s
+}
