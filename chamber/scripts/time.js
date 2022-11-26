5d457544435285e289ca6b6efd0fe10989e0e869
FelixS2000 <113263790+FelixS2000@users.noreply.github.com>
2022-11-26 18:26:08 -0400

    Update time.js
    Signed-off-by: FelixS2000 <113263790+FelixS2000@users.noreply.github.com>

--- chamber/scripts/time.js
+++ chamber/scripts/time.js
@@ -1,6 +1,14 @@
 var date = new Date();
 var year = date.getFullYear();
-var time = date.toLocaleString();
+var time1 = date.toLocaleString();
+var time2 = date.toLocaleString();
+var message;
+
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+if (days == Monday){
+    message = "🤝🏼 Come join us for the chamber meet and greet Monday at 7:00 p.m.";
+}
+
 const hambutton = document.querySelector('.navig');
 const mainnav = document.querySelector('.navigation')
 
@@ -10,6 +18,5 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
 document.getElementById("currentYear").innerHTML = year;
-document.getElementById("lastModified").innerHTML = time;
-document.getElementById("lastTime").innerHTML = time;
-document.getElementById("bannerMessage").textContent = "🤝🏼 Come join us for the chamber meet and greet Monday at 7:00 p.m.";
+document.getElementById("lastModified").innerHTML = date;
+
