018cd84e76b7c35ab07dfb5d81d0778bb7beea90
FelixS2000 <113263790+FelixS2000@users.noreply.github.com>
2022-12-16 14:12:39 -0400

    Update weather.js
    Signed-off-by: FelixS2000 <113263790+FelixS2000@users.noreply.github.com>

--- bountiful-food/script/weather.js
+++ bountiful-food/script/weather.js
@@ -1,127 +1,99 @@
-const api_url = 
-  "https://api.openweathermap.org/data/2.5/forecast?lat=18.41&lon=-70.1&appid=5446be76d7c7bdd03fa8dc5f3a9eddb6&units=imperial";
-  
-  async function getapi(url) {
-    
-    const response = await fetch(url);
-    
-    var data = await response.json();
-    console.log(data);
+const api_url = 'https://api.openweathermap.org/data/2.5/forecast?lat=18.41&lon=-70.1&appid=5446be76d7c7bdd03fa8dc5f3a9eddb6&units=imperial';
 
-    show(data);
-    show1(data);
-    show2(data);
-    show3(data);
+async function getapi(url) {
+  const response = await fetch(url);
+
+  const data = await response.json();
+
+  show(data);
+  show1(data);
+  show2(data);
+  show3(data);
 }
 // Calling that async function
 getapi(api_url);
-  
 
 // Function to define innerHTML for HTML table
 function show(data) {
-  const obj = data;
-  const myJSON = JSON.stringify(obj);
-  const temp =document.getElementById("temp");
-  temp.innerHTML = myJSON.slice(74,76);
+  const temp = document.getElementById('temp');
 
-  const myJSON2 = JSON.stringify(obj);
-  const temp2 =document.getElementById("mintemp");
-  temp2.innerHTML = myJSON2.slice(110,112);
+  temp.innerHTML = data.list[0].main.temp;
 
-  const myJSON3 = JSON.stringify(obj);
-  const temp3 =document.getElementById("maxtemp");
-  temp3.innerHTML = myJSON3.slice(127,129);
+  const temp2 = document.getElementById('mintemp');
+  temp2.innerHTML = data.list[0].main.temp_min;
 
-  const myJSON4 = JSON.stringify(obj);
-  const description =document.getElementById("figcaption");
-  description.innerHTML = myJSON4.slice(266,276);
+  const temp3 = document.getElementById('maxtemp');
+  temp3.innerHTML = data.list[0].main.temp_max;
 
-  const myJSON5 = JSON.stringify(obj);
-  const humidity =document.getElementById("humidity");
-  humidity.innerHTML = myJSON5.slice(195,197);
+  const description = document.getElementById('desc');
+  description.innerHTML = data.list[0].weather[0].description;;
 
-  const myJSON6 = JSON.stringify(obj);
-  const wind =document.getElementById("wind");
-  wind.innerHTML = myJSON6.slice(329,333);
+  const humidity = document.getElementById('humid');
+  humidity.innerHTML = data.list[0].main.humidity;
+
+  const wind = document.getElementById('wind');
+  wind.innerHTML = data.list[0].wind.speed;
 }
 
 function show1(data) {
-  const obj = data;
-  const myJSON = JSON.stringify(obj);
-  const temp =document.getElementById("temp1");
-  temp.innerHTML = myJSON.slice(74,76);
+  const temp = document.getElementById('temp1');
 
-  const myJSON2 = JSON.stringify(obj);
-  const temp2 =document.getElementById("mintemp1");
-  temp2.innerHTML = myJSON2.slice(110,112);
+  temp.innerHTML = data.list[1].main.temp;
 
-  const myJSON3 = JSON.stringify(obj);
-  const temp3 =document.getElementById("maxtemp1");
-  temp3.innerHTML = myJSON3.slice(127,129);
+  const temp2 = document.getElementById('mintemp1');
+  temp2.innerHTML = data.list[1].main.temp_min;
 
-  const myJSON4 = JSON.stringify(obj);
-  const description =document.getElementById("figcaption1");
-  description.innerHTML = myJSON4.slice(266,276);
+  const temp3 = document.getElementById('maxtemp1');
+  temp3.innerHTML = data.list[1].main.temp_max;
 
-  const myJSON5 = JSON.stringify(obj);
-  const humidity =document.getElementById("humidity1");
-  humidity.innerHTML = myJSON5.slice(195,197);
+  const description = document.getElementById('desc1');
+  description.innerHTML = data.list[1].weather[0].description;;
 
-  const myJSON6 = JSON.stringify(obj);
-  const wind =document.getElementById("wind1");
-  wind.innerHTML = myJSON6.slice(329,333);
+  const humidity = document.getElementById('humid1');
+  humidity.innerHTML = data.list[1].main.humidity;
+
+  const wind = document.getElementById('wind1');
+  wind.innerHTML = data.list[1].wind.speed;
 }
 
 function show2(data) {
-  const obj = data;
-  const myJSON = JSON.stringify(obj);
-  const temp =document.getElementById("temp2");
-  temp.innerHTML = myJSON.slice(74,76);
+  const temp = document.getElementById('temp2');
 
-  const myJSON2 = JSON.stringify(obj);
-  const temp2 =document.getElementById("mintemp2");
-  temp2.innerHTML = myJSON2.slice(110,112);
+  temp.innerHTML = data.list[2].main.temp;
 
-  const myJSON3 = JSON.stringify(obj);
-  const temp3 =document.getElementById("maxtemp2");
-  temp3.innerHTML = myJSON3.slice(127,129);
+  const temp2 = document.getElementById('mintemp2');
+  temp2.innerHTML = data.list[2].main.temp_min;
 
-  const myJSON4 = JSON.stringify(obj);
-  const description =document.getElementById("figcaption2");
-  description.innerHTML = myJSON4.slice(266,276);
+  const temp3 = document.getElementById('maxtemp2');
+  temp3.innerHTML = data.list[2].main.temp_max;
 
-  const myJSON5 = JSON.stringify(obj);
-  const humidity =document.getElementById("humidity2");
-  humidity.innerHTML = myJSON5.slice(195,197);
+  const description = document.getElementById('desc2');
+  description.innerHTML = data.list[2].weather[0].description;;
 
-  const myJSON6 = JSON.stringify(obj);
-  const wind =document.getElementById("wind2");
-  wind.innerHTML = myJSON6.slice(329,333);
+  const humidity = document.getElementById('humid2');
+  humidity.innerHTML = data.list[2].main.humidity;
+
+  const wind = document.getElementById('wind2');
+  wind.innerHTML = data.list[2].wind.speed;
 }
 
 function show3(data) {
-  const obj = data;
-  const myJSON = JSON.stringify(obj);
-  const temp =document.getElementById("temp3");
-  temp.innerHTML = myJSON.slice(74,76);
+  const temp = document.getElementById('temp3');
 
-  const myJSON2 = JSON.stringify(obj);
-  const temp2 =document.getElementById("mintemp3");
-  temp2.innerHTML = myJSON2.slice(110,112);
+  temp.innerHTML = data.list[3].main.temp;
 
-  const myJSON3 = JSON.stringify(obj);
-  const temp3 =document.getElementById("maxtemp3");
-  temp3.innerHTML = myJSON3.slice(127,129);
+  const temp2 = document.getElementById('mintemp3');
+  temp2.innerHTML = data.list[3].main.temp_min;
 
-  const myJSON4 = JSON.stringify(obj);
-  const description =document.getElementById("figcaption3");
-  description.innerHTML = myJSON4.slice(266,276);
+  const temp3 = document.getElementById('maxtemp3');
+  temp3.innerHTML = data.list[3].main.temp_max;
 
-  const myJSON5 = JSON.stringify(obj);
-  const humidity =document.getElementById("humidity");
-  humidity.innerHTML = myJSON5.slice(195,197);
+  const description = document.getElementById('desc3');
+  description.innerHTML = data.list[3].weather[0].description;;
 
-  const myJSON6 = JSON.stringify(obj);
-  const wind =document.getElementById("wind");
-  wind.innerHTML = myJSON6.slice(329,333);
+  const humidity = document.getElementById('humid3');
+  humidity.innerHTML = data.list[3].main.humidity;
+
+  const wind = document.getElementById('wind3');
+  wind.innerHTML = data.list[3].wind.speed;
 }
