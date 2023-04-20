eb9403ffcd79ae6745d785ff153b9800165bf658
Felix <fguzmanceri@gmail.com>
2023-04-20 10:42:15 -0400

    saving link for my codepen and update for time

--- scripts/year.js
+++ scripts/year.js
@@ -1,4 +1,6 @@
 const date = new Date();
 const year = date.getFullYear();
-document.getElementById("currentTime").innerHTML = document.lastModified;
+const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+const formattedDate = date.toLocaleDateString('en-US', options);
+document.getElementById("currentTime").innerHTML = formattedDate;
 document.getElementById("currentYear").innerHTML = year;
