eb292e0fe17150909113ace293e244861780bc61
Felix <fguzmanceri@gmail.com>
2023-05-05 16:14:08 -0400

    saving menu updates

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -2,5 +2,6 @@ const hamburgerButton = document.getElementById("hamButton");
 const navigation = document.querySelector(".navigation");
 
 hamburgerButton.addEventListener("click", function() {
+  console.log("Clicked!");
   navigation.classList.toggle("responsive");
 });
