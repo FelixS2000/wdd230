150937970b4a9a1396e6c9db43ffa50cb9491c0c
Felix <fguzmanceri@gmail.com>
2023-05-15 19:44:45 -0400

    display banner message for the chamber page

--- chamber/scripts/time.js
+++ chamber/scripts/time.js
@@ -2,8 +2,17 @@ var date = new Date();
 var currentYear = date.getFullYear();
 var currentDate = date.getDate();
 var currentMonth = date.getMonth() + 1;
+var currentDay = date.getDay();
 var options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'};
 var formattedDate = date.toLocaleDateString('en-US', options);
+
 document.getElementById("currentYear").textContent = currentYear;
 document.getElementById("lastModified").textContent = document.lastModified;
 document.getElementById("lastTime").textContent = formattedDate;
+
+if (currentDay === 1) {
+  var banner = document.createElement("div");
+  banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+  banner.classList.add("banner");
+  document.body.insertBefore(banner, document.body.firstChild);
+}
