a0ae5de6198fe1196d8efdcae04b4ab151ae2774
Felix <fguzmanceri@gmail.com>
2023-05-22 09:18:21 -0400

    saving windchill in page

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,19 @@
+// Get the temperature and wind speed values from the HTML document
+const temperatureValue = document.getElementById("temperature-value").textContent;
+const windSpeedValue = document.getElementById("wind-speed-value").textContent;
+
+// Convert the temperature and wind speed values to numbers
+const temperature = parseFloat(temperatureValue);
+const windSpeed = parseFloat(windSpeedValue);
+
+// Check if the values meet the specification limits for wind chill calculation
+if (temperature <= 50 && windSpeed > 3.0) {
+  // Calculate the wind chill factor using the provided formula
+  const windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+  
+  // Update the HTML document to display the wind chill value
+  document.getElementById("wind-chill-value").textContent = windChill.toFixed(2);
+} else {
+  // Display "N/A" if the input values do not meet the requirements for wind chill calculation
+  document.getElementById("wind-chill-value").textContent = "N/A";
+}
