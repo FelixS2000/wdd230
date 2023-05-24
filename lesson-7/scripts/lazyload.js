28716af69cd855e5c69db277fb51026b99010d06
Felix <fguzmanceri@gmail.com>
2023-05-24 16:00:01 -0400

    saving updates

--- lesson-7/scripts/lazyload.js
+++ lesson-7/scripts/lazyload.js
@@ -1,34 +1,37 @@
 function lazyLoad() {
     const images = document.querySelectorAll(".lazy");
-
+  
     const options = {
       root: null,
       rootMargin: "0px",
       threshold: 0.2
     };
-
-    const intersectionObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(entry => {
+  
+    const intersectionObserver = new IntersectionObserver(function (entries, observer) {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
           const image = entry.target;
           const src = image.getAttribute("data-src");
           const srcset = image.getAttribute("data-srcset");
-
+  
           if (src) {
             image.src = src;
           }
-
+  
           if (srcset) {
             image.srcset = srcset;
           }
-
+  
           image.classList.add("loaded");
           intersectionObserver.unobserve(image);
         }
       });
     }, options);
-
-    images.forEach(image => {
+  
+    images.forEach((image) => {
       intersectionObserver.observe(image);
     });
   }
+  
+  window.addEventListener("load", lazyLoad);
+  
