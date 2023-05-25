42297af10d0dfe4dc49a7cc25ee611997d927bb8
Felix <fguzmanceri@gmail.com>
2023-05-24 20:39:27 -0400

    saving js and css

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -37,3 +37,21 @@ if (visitTime) {
 }
 
 localStorage.setItem("visitTime", currentTime);
+
+var date = new Date();
+var currentYear = date.getFullYear();
+var currentDates = date.getDate();
+var currentMonth = date.getMonth() + 1;
+var currentDay = date.getDay();
+var option = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'};
+var formattedDate = date.toLocaleDateString('en-US', options);
+
+document.getElementById("currentYear").textContent = currentYear;
+document.getElementById("lastTime").textContent = document.lastModified;
+
+if (currentDay === 1) {
+  var banner = document.createElement("div");
+  banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+  banner.classList.add("banner");
+  document.body.insertBefore(banner, document.body.firstChild);
+}
