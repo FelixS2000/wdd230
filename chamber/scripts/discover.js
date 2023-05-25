4e3611138a6e79d931b493347c198fb29b190769
Felix <fguzmanceri@gmail.com>
2023-05-25 13:46:14 -0400

    saving js

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -27,49 +27,34 @@ images.forEach((image) => {
 });
 
 // Local Storage Visit Time
-const visitTime = localStorage.getItem("visitTime");
-const currentDate = new Date();
-const currentTime = currentDate.getTime();
+const lastVisitTime = localStorage.getItem("lastVisitTime");
+const currentTime = new Date().getTime();
 
-if (visitTime) {
-  const daysSinceLastVisit = Math.round((currentTime - visitTime) / (1000 * 60 * 60 * 24));
+if (lastVisitTime) {
+  const oneDayMilliseconds = 24 * 60 * 60 * 1000;
+  const daysSinceLastVisit = Math.round((currentTime - lastVisitTime) / oneDayMilliseconds);
   console.log(`Days since last visit: ${daysSinceLastVisit}`);
+
+  const lastTimeElement = document.getElementById('lastTime');
+  if (lastTimeElement) {
+    lastTimeElement.textContent = `Days since last visit: ${daysSinceLastVisit}`;
+  }
 }
 
-localStorage.setItem("visitTime", currentTime);
+localStorage.setItem("lastVisitTime", currentTime);
 
-var date = new Date();
-var currentYear = date.getFullYear();
-var currentDates = date.getDate();
-var currentMonth = date.getMonth() + 1;
-var currentDay = date.getDay();
-var option = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'};
-var formattedDate = date.toLocaleDateString('en-US', options);
+// Display current date
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth() + 1;
+const currentDay = currentDate.getDate();
 
 document.getElementById("currentYear").textContent = currentYear;
-document.getElementById("lastTime").textContent = document.lastModified;
-document.getElementById("lastTimeModified").textContent = document.lastModified;
 
+// Check if it's Monday (day 1)
 if (currentDay === 1) {
-  var banner = document.createElement("div");
+  const banner = document.createElement("div");
   banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
   banner.classList.add("banner");
   document.body.insertBefore(banner, document.body.firstChild);
 }
-
- // Retrieve the last visit time from local storage
- const lastVisitTime = localStorage.getItem('lastVisitTime');
- const currentTimes = new Date().getTime();
-
- // Calculate the number of days between visits
- const oneDayMilliseconds = 24 * 60 * 60 * 1000;
- const daysSinceLastVisit = Math.round((currentTime - lastVisitTime) / oneDayMilliseconds);
-
- // Display the number of days on the page
- const lastTimeElement = document.getElementById('lastTime');
- if (lastTimeElement) {
-   lastTimeElement.textContent = `Days since last visit: ${daysSinceLastVisit}`;
- }
-
- // Store the current visit time in local storage
- localStorage.setItem('lastVisitTime', currentTimes);
