61c7155d905d8a28240baa428cdce3f5b8204988
Felix <fguzmanceri@gmail.com>
2023-05-31 12:27:17 -0400

    saving join javascript and css

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -4,13 +4,13 @@ const currentYear = currentDate.getFullYear();
 const currentMonth = currentDate.getMonth() + 1;
 const currentDay = currentDate.getDay();
 
-const option = { weekday: "long", month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric" };
+const option = { weekday: "long", month: "long", day: "numeric", year: "numeric"};
 const lastTimeModifiedElement = document.getElementById("lastTimeModified");
 if (lastTimeModifiedElement) {
   lastTimeModifiedElement.textContent = currentDate.toLocaleDateString("en-US", option);
 }
 document.getElementById("currentYear").textContent = currentYear;
-
+document.getElementById("lastTime").textContent = currentDate.toLocaleDateString("en-US", option);
 // Check if it's Monday (day 1)
 if (currentDay === 1) {
   const banner = document.createElement("div");
