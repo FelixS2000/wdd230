07bbdb9c3a7aa69fa2b9a8dd0b2cd0f5c737ce03
Felix <fguzmanceri@gmail.com>
2023-05-31 12:21:55 -0400

    saving join page and css

---
+++ chamber/scripts/join.js
@@ -0,0 +1,24 @@
+// Display current date
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth() + 1;
+const currentDay = currentDate.getDay();
+
+const option = { weekday: "long", month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric" };
+const lastTimeModifiedElement = document.getElementById("lastTimeModified");
+if (lastTimeModifiedElement) {
+  lastTimeModifiedElement.textContent = currentDate.toLocaleDateString("en-US", option);
+}
+document.getElementById("currentYear").textContent = currentYear;
+
+// Check if it's Monday (day 1)
+if (currentDay === 1) {
+  const banner = document.createElement("div");
+  banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+  banner.classList.add("banner");
+  document.body.insertBefore(banner, document.body.firstChild);
+}
+
+// Set the current date and time in the hidden field when the form loads
+const formLoadedDateTimeField = document.querySelector('input[name="formLoadedDateTime"]');
+formLoadedDateTimeField.value = new Date().toISOString();
