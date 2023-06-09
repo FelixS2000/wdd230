5bc2a654e5f6fad3d81cec28dab5d85e9b0f9c1e
Felix <fguzmanceri@gmail.com>
2023-06-09 11:59:54 -0400

    saving images for directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -10,6 +10,7 @@ fetch('scripts/data.json')
       const companyHTML = `
         <div class="company">
           <h2>${company['Company Name']}</h2>
+          <img src="images/${company['Image/Icon']}" alt="${company['Company Name']} Logo">
           <p><strong>Address:</strong> ${company.Address}</p>
           <p><strong>Phone Number:</strong> ${company['Phone Number']}</p>
           <p><strong>Website:</strong> <a href="http://${company.Website}" target="_blank">${company.Website}</a></p>
@@ -21,3 +22,25 @@ fetch('scripts/data.json')
       companyList.innerHTML += companyHTML;
     });
   });
+
+
+  // Display current date
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth() + 1;
+const currentDay = currentDate.getDay();
+
+const option = { weekday: "long", month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric" };
+const lastTimeModifiedElement = document.getElementById("lastTimeModified");
+if (lastTimeModifiedElement) {
+  lastTimeModifiedElement.textContent = currentDate.toLocaleDateString("en-US", option);
+}
+document.getElementById("currentYear").textContent = currentYear;
+
+// Check if it's Monday (day 1)
+if (currentDay === 1) {
+  const banner = document.createElement("div");
+  banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+  banner.classList.add("banner");
+  document.body.insertBefore(banner, document.body.firstChild);
+}
