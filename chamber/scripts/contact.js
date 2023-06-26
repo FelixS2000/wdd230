14cac8ecff81d6fc95ab18fb5b6a0f5dc7bf14de
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2023-06-26 18:24:14 -0400

    saving updates for contact pages

--- chamber/scripts/contact.js
+++ chamber/scripts/contact.js
@@ -15,3 +15,24 @@ form.addEventListener('submit', function (event) {
     // Reset the form after submission
     form.reset();
 });
+  // Display current date
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentDay = currentDate.getDay();
+  
+  const option = { weekday: "long", month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric" };
+  const lastTimeModifiedElement = document.getElementById("lastTimeModified");
+  if (lastTimeModifiedElement) {
+    lastTimeModifiedElement.textContent = currentDate.toLocaleDateString("en-US", option);
+  }
+  document.getElementById("currentYear").textContent = currentYear;
+  
+  // Check if it's Monday (day 1)
+  if (currentDay === 1) {
+    const banner = document.createElement("div");
+    banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    banner.classList.add("banner");
+    document.body.insertBefore(banner, document.body.firstChild);
+  }
+  
