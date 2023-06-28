aaa5b2d46a38f9c2c52fa280d6356c4e36db03d7
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2023-06-28 12:47:41 -0400

    saving updates for spotlight

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,22 +1,42 @@
-// Fetch the JSON data
-fetch('company.json')
+fetch('scripts/data.json')
   .then(response => response.json())
   .then(data => {
-    // Update the first spotlight section
-    const firstSpotlight = document.getElementById('firstSpotlight');
-    const firstSpotlightHeading = firstSpotlight.querySelector('h3');
-    firstSpotlightHeading.textContent = data.companies[0]['Additional Information'];
+    const spotlight1 = document.getElementById('firstSpotlight');
+    const spotlight2 = document.getElementById('secondSpotlight');
+    const spotlight3 = document.getElementById('thirdSpotlight');
 
-    // Update the second spotlight section
-    const secondSpotlight = document.getElementById('secondSpotlight');
-    const secondSpotlightHeading = secondSpotlight.querySelector('h3');
-    secondSpotlightHeading.textContent = data.companies[1]['Additional Information'];
+    // Filter companies with silver or gold status
+    const silverGoldMembers = data.companies.filter(company => company['Membership Level'] === 'Silver' || company['Membership Level'] === 'Gold');
 
-    // Update the third spotlight section
-    const thirdSpotlight = document.getElementById('thirdSpotlight');
-    const thirdSpotlightHeading = thirdSpotlight.querySelector('h3');
-    thirdSpotlightHeading.textContent = data.companies[2]['Additional Information'];
-  })
-  .catch(error => {
-    console.error('Error fetching JSON data:', error);
+    // Randomly select two to three members from silverGoldMembers
+    const selectedMembers = getRandomMembers(silverGoldMembers, 2, 3);
+
+    // Update the HTML content for each spotlight
+    selectedMembers.forEach((member, index) => {
+      const spotlight = index === 0 ? spotlight1 : index === 1 ? spotlight2 : spotlight3;
+      spotlight.innerHTML = `
+        <h2>${member['Company Name']}</h2>
+        <img src="${member['Image/Icon']}" alt="${member['Company Name']} Logo">
+        <p><strong>Address:</strong> ${member.Address}</p>
+        <p><strong>Phone Number:</strong> ${member['Phone Number']}</p>
+        <p><strong>Website:</strong> <a href="http://${member.Website}" target="_blank">${member.Website}</a></p>
+        <p><strong>Membership Level:</strong> ${member['Membership Level']}</p>
+        <p><strong>Additional Information:</strong> ${member['Additional Information']}</p>
+      `;
+    });
   });
+
+// Function to randomly select members from an array
+function getRandomMembers(array, min, max) {
+  const selectedMembers = [];
+  const numMembers = Math.floor(Math.random() * (max - min + 1)) + min;
+  const arrayCopy = [...array];
+
+  for (let i = 0; i < numMembers; i++) {
+    const randomIndex = Math.floor(Math.random() * arrayCopy.length);
+    const selectedMember = arrayCopy.splice(randomIndex, 1)[0];
+    selectedMembers.push(selectedMember);
+  }
+
+  return selectedMembers;
+}
