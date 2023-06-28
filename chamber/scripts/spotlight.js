4ddc8bf2b3498e73d78534bc2bbe0fc93a3a9d25
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2023-06-28 11:58:19 -0400

    saving updates

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,44 +1,22 @@
-// Fetch the data from the JSON source
-fetch('data.json')
+// Fetch the JSON data
+fetch('company.json')
   .then(response => response.json())
   .then(data => {
-    // Get the spotlight sections
-    const firstSpotlight = document.getElementById('firstSpotlight');
-    const secondSpotlight = document.getElementById('secondSpotlight');
-    const thirdSpotlight = document.getElementById('thirdSpotlight');
-
-    // Filter the companies with silver or gold status
-    const silverGoldCompanies = data.companies.filter(company =>
-      company['Membership Level'] === 'Silver Membership' || company['Membership Level'] === 'Gold Membership'
-    );
-
-    // Randomly select two or three companies from the filtered list
-    const selectedCompanies = getRandomCompanies(silverGoldCompanies, 2, 3);
-
     // Update the first spotlight section
-    updateSpotlight(firstSpotlight, selectedCompanies[0]);
+    const firstSpotlight = document.getElementById('firstSpotlight');
+    const firstSpotlightHeading = firstSpotlight.querySelector('h3');
+    firstSpotlightHeading.textContent = data.companies[0]['Additional Information'];
 
     // Update the second spotlight section
-    updateSpotlight(secondSpotlight, selectedCompanies[1]);
+    const secondSpotlight = document.getElementById('secondSpotlight');
+    const secondSpotlightHeading = secondSpotlight.querySelector('h3');
+    secondSpotlightHeading.textContent = data.companies[1]['Additional Information'];
 
-    // Update the third spotlight section if a third company is available
-    if (selectedCompanies.length > 2) {
-      updateSpotlight(thirdSpotlight, selectedCompanies[2]);
-    }
+    // Update the third spotlight section
+    const thirdSpotlight = document.getElementById('thirdSpotlight');
+    const thirdSpotlightHeading = thirdSpotlight.querySelector('h3');
+    thirdSpotlightHeading.textContent = data.companies[2]['Additional Information'];
+  })
+  .catch(error => {
+    console.error('Error fetching JSON data:', error);
   });
-
-// Function to randomly select companies from the list
-function getRandomCompanies(companies, minCount, maxCount) {
-  const count = Math.floor(Math.random() * (maxCount - minCount + 1)) + minCount;
-  const shuffledCompanies = companies.sort(() => 0.5 - Math.random());
-  return shuffledCompanies.slice(0, count);
-}
-
-// Function to update the spotlight section with company information
-function updateSpotlight(spotlight, company) {
-  spotlight.innerHTML = `
-    <h2>${company['Company Name']}</h2>
-    <p>${company['Additional Information']}</p>
-    <img src="${company['Image/Icon']}" alt="${company['Company Name']}">
-  `;
-}
