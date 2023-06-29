66b8dc162345e568a556d11e042daa22ca11aac9
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2023-06-29 10:15:28 -0400

    saving spotlight js

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,54 +1,58 @@
-// Randomly select companies with silver or gold membership
-function getRandomMembers(companies, membershipLevel, count) {
-    const filteredCompanies = companies.filter(company => company["Membership Level"] === membershipLevel);
-    const randomMembers = [];
-    const maxIndex = filteredCompanies.length - 1;
-  
-    for (let i = 0; i < count; i++) {
-      const randomIndex = Math.floor(Math.random() * (maxIndex + 1));
-      randomMembers.push(filteredCompanies[randomIndex]);
-    }
-  
-    return randomMembers;
+// Fetch the JSON data
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Filter companies with silver or gold membership level
+    const silverAndGoldCompanies = data.companies.filter(company =>
+      company['Membership Level'] === 'Silver Membership' ||
+      company['Membership Level'] === 'Gold Membership'
+    );
+
+    // Shuffle the array randomly
+    shuffleArray(silverAndGoldCompanies);
+
+    // Display companies in the spotlights
+    const firstSpotlight = document.getElementById('firstSpotlight');
+    const secondSpotlight = document.getElementById('secondSpotlight');
+    const thirdSpotlight = document.getElementById('thirdSpotlight');
+
+    displayCompany(firstSpotlight, silverAndGoldCompanies[0]);
+    displayCompany(secondSpotlight, silverAndGoldCompanies[1]);
+    displayCompany(thirdSpotlight, silverAndGoldCompanies[2]);
+  });
+
+// Function to shuffle an array randomly
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
-  
-  // Display random members in the spotlights
-  function displaySpotlights(companies) {
-    const firstSpotlight = document.getElementById("firstSpotlight");
-    const secondSpotlight = document.getElementById("secondSpotlight");
-    const thirdSpotlight = document.getElementById("thirdSpotlight");
-  
-    // First Spotlight
-    const firstRandomMembers = getRandomMembers(companies, "Silver Membership", 1);
-    const firstSpotlightHTML = `
-      <h2>${firstRandomMembers[0]["Company Name"]}</h2>
-      <p>${firstRandomMembers[0]["Additional Information"]}</p>
-    `;
-    firstSpotlight.innerHTML = firstSpotlightHTML;
-  
-    // Second Spotlight
-    const secondRandomMembers = getRandomMembers(companies, "Gold Membership", 1);
-    const secondSpotlightHTML = `
-      <h2>${secondRandomMembers[0]["Company Name"]}</h2>
-      <p>${secondRandomMembers[0]["Additional Information"]}</p>
-    `;
-    secondSpotlight.innerHTML = secondSpotlightHTML;
-  
-    // Third Spotlight
-    const thirdRandomMembers = getRandomMembers(companies, "Silver Membership", 1);
-    const thirdSpotlightHTML = `
-      <h2>${thirdRandomMembers[0]["Company Name"]}</h2>
-      <p>${thirdRandomMembers[0]["Additional Information"]}</p>
-    `;
-    thirdSpotlight.innerHTML = thirdSpotlightHTML;
-  }
-  
-  // Fetch and process the data
-  fetch("data.json")
-    .then(response => response.json())
-    .then(data => {
-      const companies = data.companies;
-      displaySpotlights(companies);
-    })
-    .catch(error => console.log(error));
-  
+}
+
+// Function to display a company in a spotlight section
+function displayCompany(spotlightSection, company) {
+  const h2 = document.createElement('h2');
+  h2.textContent = company['Company Name'];
+
+  const h3 = document.createElement('h3');
+  h3.textContent = company['Additional Information'];
+
+  const img = document.createElement('img');
+  img.src = company['Image/Icon'];
+  img.alt = company['Company Name'];
+
+  const p = document.createElement('p');
+  p.textContent = 'Website: ';
+
+  const a = document.createElement('a');
+  a.href = company['Website'];
+  a.textContent = company['Website'];
+  a.target = '_blank';
+
+  p.appendChild(a);
+
+  spotlightSection.appendChild(h2);
+  spotlightSection.appendChild(h3);
+  spotlightSection.appendChild(img);
+  spotlightSection.appendChild(p);
+}
