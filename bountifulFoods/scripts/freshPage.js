8e1a0c21ec6b997d3ee4c3b94a4085b3923c5e0e
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2023-07-05 16:15:39 -0400

    saving updating freshpage for fruit

--- bountifulFoods/scripts/freshPage.js
+++ bountifulFoods/scripts/freshPage.js
@@ -3,6 +3,10 @@ async function fetchFruitData() {
     try {
       const response = await fetch('https://brotherblazzard.github.io/canvas-content/fruit.json');
       const data = await response.json();
+      const fruitOptions = data.map(fruit => `<option value="${fruit.name}">${fruit.name}</option>`);
+      document.getElementById('fruit1').innerHTML = fruitOptions.join('');
+      document.getElementById('fruit2').innerHTML = fruitOptions.join('');
+      document.getElementById('fruit3').innerHTML = fruitOptions.join('');
       return data;
     } catch (error) {
       console.log('Error fetching fruit data:', error);
