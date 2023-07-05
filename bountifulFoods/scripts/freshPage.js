c3170853c8fca687b387c62ebd3df8a36ca0c788
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2023-07-05 16:32:03 -0400

    saving freshpage

--- bountifulFoods/scripts/freshPage.js
+++ bountifulFoods/scripts/freshPage.js
@@ -15,7 +15,7 @@ async function fetchFruitData() {
   }
   
   // Function to calculate the total nutritional values based on selected fruits
-function calculateTotalNutrition(fruitData, fruitSelections) {
+  function calculateTotalNutrition(fruitData, fruitSelections) {
     let totalCarbs = 0;
     let totalProtein = 0;
     let totalFat = 0;
@@ -53,7 +53,6 @@ function calculateTotalNutrition(fruitData, fruitSelections) {
     };
   }
   
-  
   // Function to handle form submission
   function handleFormSubmit(event) {
     event.preventDefault();
@@ -69,35 +68,33 @@ function calculateTotalNutrition(fruitData, fruitSelections) {
     const fruitSelections = [fruit1, fruit2, fruit3];
   
     fetchFruitData().then(fruitData => {
-        const totalNutrition = calculateTotalNutrition(fruitData, fruitSelections);
-        const currentDate = new Date().toLocaleDateString();
-    
-        // Create the formatted output
-        let output = `
-          <h3>Order Details:</h3>
-          <p><strong>First Name:</strong> ${firstName}</p>
-          <p><strong>Email:</strong> ${email}</p>
-          <p><strong>Phone Number:</strong> ${phoneNumber}</p>
-          <p><strong>Fruit 1:</strong> ${fruit1}</p>
-          <p><strong>Fruit 2:</strong> ${fruit2}</p>
-          <p><strong>Fruit 3:</strong> ${fruit3}</p>
-          <p><strong>Special Instructions:</strong> ${specialInstructions}</p>
-    
-          <h3>Nutritional Information:</h3>
-          <p><strong>Total Carbohydrates:</strong> ${totalNutrition.totalCarbs} g</p>
-          <p><strong>Total Protein:</strong> ${totalNutrition.totalProtein} g</p>
-          <p><strong>Total Fat:</strong> ${totalNutrition.totalFat} g</p>
-          <p><strong>Total Sugar:</strong> ${totalNutrition.totalSugar} g</p>
-          <p><strong>Total Calories:</strong> ${totalNutrition.totalCalories} cal</p>
-    
-          <p><strong>Order Date:</strong> ${currentDate}</p>
+      const totalNutrition = calculateTotalNutrition(fruitData, fruitSelections);
+      const currentDate = new Date().toLocaleDateString();
+  
+      // Create the formatted output
+      let output = `
+        <h3>Order Details:</h3>
+        <p><strong>First Name:</strong> ${firstName}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone Number:</strong> ${phoneNumber}</p>
+        <p><strong>Fruit 1:</strong> ${fruit1}</p>
+        <p><strong>Fruit 2:</strong> ${fruit2}</p>
+        <p><strong>Fruit 3:</strong> ${fruit3}</p>
+        <p><strong>Special Instructions:</strong> ${specialInstructions}</p>
+  
+        <h3>Nutritional Information:</h3>
+        <p><strong>Total Carbohydrates:</strong> ${totalNutrition.totalCarbs} g</p>
+        <p><strong>Total Sugar:</strong> ${totalNutrition.totalSugar} g</p>
+        <p><strong>Total Calories:</strong> ${totalNutrition.totalCalories} cal</p>
         `;
-    
-        document.getElementById('orderSummary').innerHTML = output;
-      })
-      .catch(error => {
-        console.log('Error:', error);
-        document.getElementById('orderSummary').innerHTML = '<p>Error processing the order. Please try again.</p>';
-      });
-    }
-      
+
+        // Get the output element
+        const outputElement = document.getElementById('output');
+
+        // Display the formatted output
+        outputElement.innerHTML = output;
+        });
+        }
+
+        // Add an event listener to the form submit button
+        document.getElementById('orderForm').addEventListener('submit', handleFormSubmit);
