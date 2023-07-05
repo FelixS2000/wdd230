b1ce25b4bb395ff7ea69bbcc37c2ea3d23cf0345
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2023-07-05 16:50:53 -0400

    saving freshpage

--- bountifulFoods/scripts/freshPage.js
+++ bountifulFoods/scripts/freshPage.js
@@ -1,18 +1,80 @@
-async function fetchFruitData() {
+// Function to calculate the total nutritional values based on selected fruits
+function calculateTotalNutrition(fruitData, fruitSelections) {
+    // Initialize variables for total nutritional values
+    let totalCalories = 0;
+    let totalCarbs = 0;
+    let totalProtein = 0;
+    let totalFat = 0;
+  
+    // Iterate over the fruit selections
+    fruitSelections.forEach(selection => {
+      // Find the selected fruit in the fruit data
+      const fruit = fruitData.find(item => item.name === selection);
+  
+      // If the fruit is found, update the total nutritional values
+      if (fruit) {
+        totalCalories += fruit.nutrition.calories || 0;
+        totalCarbs += fruit.nutrition.carbs || 0;
+        totalProtein += fruit.nutrition.protein || 0;
+        totalFat += fruit.nutrition.fat || 0;
+      }
+    });
+  
+    // Return an object containing the total nutritional values
+    return {
+      totalCalories,
+      totalCarbs,
+      totalProtein,
+      totalFat
+    };
+  }
+  
+  // Function to handle form submission
+  function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the form from submitting and refreshing the page
+  
+    // Get the selected fruit values from the form
+    const fruit1 = document.getElementById('fruit1').value;
+    const fruit2 = document.getElementById('fruit2').value;
+    const fruit3 = document.getElementById('fruit3').value;
+  
+    // Create an array of the selected fruit values
+    const fruitSelections = [fruit1, fruit2, fruit3];
+  
+    // Call the calculateTotalNutrition function with the fruit data and selections
+    const totalNutrition = calculateTotalNutrition(fruitData, fruitSelections);
+  
+    // Display the total nutritional values in the output div
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = `
+      <p>Total Calories: ${totalNutrition.totalCalories}</p>
+      <p>Total Carbs: ${totalNutrition.totalCarbs}</p>
+      <p>Total Protein: ${totalNutrition.totalProtein}</p>
+      <p>Total Fat: ${totalNutrition.totalFat}</p>
+    `;
+  }
+  
+  // Add an event listener to the form submit button
+  document.getElementById('orderForm').addEventListener('submit', handleFormSubmit);
+  
+  // Fetch fruit data when the page loads
+  let fruitData;
+  
+  async function fetchFruitData() {
     try {
       const response = await fetch('https://brotherblazzard.github.io/canvas-content/fruit.json');
-      const data = await response.json();
+      fruitData = await response.json();
       
       // Populate select elements with fruit options
-      const fruitOptions = data.map(fruit => `<option value="${fruit.name}">${fruit.name}</option>`);
+      const fruitOptions = fruitData.map(fruit => `<option value="${fruit.name}">${fruit.name}</option>`);
       document.getElementById('fruit1').innerHTML = fruitOptions.join('');
       document.getElementById('fruit2').innerHTML = fruitOptions.join('');
       document.getElementById('fruit3').innerHTML = fruitOptions.join('');
-  
-      return data;
     } catch (error) {
       console.log('Error fetching fruit data:', error);
-      return [];
     }
   }
+  
+  // Fetch fruit data when the page loads
+  fetchFruitData();
   
