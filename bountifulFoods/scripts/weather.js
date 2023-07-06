3344fb2cc9b12b1440354d77c73f97c70070667b
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2023-07-06 19:18:17 -0400

    saving weather js

--- bountifulFoods/scripts/weather.js
+++ bountifulFoods/scripts/weather.js
@@ -11,17 +11,18 @@
    humidity.textContent = `Humidity: ${data.main.humidity}%`;
  });
 
-// Fetch the three-day forecast data
-fetch('https://api.openweathermap.org/data/2.5/forecast/daily?q=Carlsbad&cnt=3&appid=5446be76d7c7bdd03fa8dc5f3a9eddb6')
- .then(response => response.json())
- .then(data => {
-   const forecast = data.list;
-   const day1 = document.getElementById('day1');
-   const day2 = document.getElementById('day2');
-   const day3 = document.getElementById('day3');
+ document.addEventListener('DOMContentLoaded', function() {
+  fetch('https://api.openweathermap.org/data/2.5/forecast/daily?q=Carlsbad&cnt=3&appid=5446be76d7c7bdd03fa8dc5f3a9eddb6')
+    .then(response => response.json())
+    .then(data => {
+      const forecast = data.list;
+      const day1 = document.getElementById('day1');
+      const day2 = document.getElementById('day2');
+      const day3 = document.getElementById('day3');
 
-   // Extract and display the temperature for each day
-   day1.textContent = `Day 1: ${Math.round(forecast[0].temp.day - 273.15)}°C`;
-   day2.textContent = `Day 2: ${Math.round(forecast[1].temp.day - 273.15)}°C`;
-   day3.textContent = `Day 3: ${Math.round(forecast[2].temp.day - 273.15)}°C`;
- });
+      // Extract and display the temperature for each day
+      day1.textContent = `Day 1: ${Math.round(forecast[0].temp.day - 273.15)}°C`;
+      day2.textContent = `Day 2: ${Math.round(forecast[1].temp.day - 273.15)}°C`;
+      day3.textContent = `Day 3: ${Math.round(forecast[2].temp.day - 273.15)}°C`;
+    });
+});
