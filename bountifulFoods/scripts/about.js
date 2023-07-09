edbac9f9564023aa02415e6e3efd13228469b71a
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2023-07-08 23:49:38 -0400

    saving

--- bountifulFoods/scripts/about.js
+++ bountifulFoods/scripts/about.js
@@ -1,3 +1,3 @@
 const lastModifiedDate = document.lastModified;
-const footer = document.querySelector("span");
+const footer = document.getElementById("lastTime");
 footer.innerHTML += `<p>Last Modified: ${lastModifiedDate}</p>`;
